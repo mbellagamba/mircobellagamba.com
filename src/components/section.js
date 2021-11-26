12be07d19424ca27cf0ec13b9018a93454753f0d
Mirco Bellagamba <mirco.bellag@gmail.com>
2021-11-26 08:58:36 +0100

    Add animations to sections

--- src/components/section.js
+++ src/components/section.js
@@ -1,13 +1,24 @@
 import React from "react"
+import useIntersectionObserver from "../hooks/useIntersectionObserver"
+import * as styles from "./section.module.css"
 
 export default function Section({ title, className, children, style }) {
-  const divClass = "card" + (className ? " " + className : "")
+  const sectionRef = React.useRef(null)
+  const entry = useIntersectionObserver(sectionRef, { freezeOnceVisible: true })
+  const isVisible = !!entry?.isIntersecting
   return (
-    <>
+    <section
+      ref={sectionRef}
+      className={classNames(styles.section, isVisible && styles.isVisible)}
+    >
       <h2 className="big-section">{title}.</h2>
-      <div className={divClass} style={style}>
+      <div className={classNames("card", className)} style={style}>
         {children}
       </div>
-    </>
+    </section>
   )
+}
+
+function classNames(...names) {
+  return names.filter(Boolean).join(" ")
 }
