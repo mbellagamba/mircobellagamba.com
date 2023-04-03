d3f7ec278e865b9c03f721ea8f1ea29ae9a6a557
Mirco Bellagamba <mirco.bellag@gmail.com>
2023-04-03 19:33:30 +0200

    Add cookies alert

---
+++ public/js/cookies.js
@@ -0,0 +1,77 @@
+const ACCEPTS_COOKIES_KEY = "web-accepts-cookies";
+const GOOGLE_TAG_ID = "G-W2E9LR5DLD";
+
+function appendScript({ src, content, async }) {
+	const script = document.createElement("script");
+	if (src) script.src = src;
+	if (content) script.innerHTML = content;
+	script.async = async;
+	document.body.appendChild(script);
+}
+
+function addAnalytics() {
+	appendScript({
+		src: `https://www.googletagmanager.com/gtag/js?id=${GOOGLE_TAG_ID}`,
+		async: true,
+	});
+	appendScript({
+		content: `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', '${GOOGLE_TAG_ID}');`,
+	});
+}
+
+function hideCookiesAlert() {
+	const cookiesAlert = document.querySelector(".cookies");
+	cookiesAlert.classList.add("cookies--hidden");
+}
+
+function showCookiesAlert() {
+	const cookiesAlert = document.querySelector(".cookies");
+	cookiesAlert.classList.remove("cookies--hidden");
+}
+
+function askPermissionForAnalytics() {
+	if (!isCookiesPermissionAsked()) {
+		showCookiesAlert();
+	} else if (isCookiesAccepted()) {
+		addAnalytics();
+	}
+}
+
+function isCookiesPermissionAsked() {
+	return document.cookie
+		.split(";")
+		.some((cookie) => cookie.startsWith(ACCEPTS_COOKIES_KEY));
+}
+
+function isCookiesAccepted() {
+	return document.cookie.split(";").includes(`${ACCEPTS_COOKIES_KEY}=1`);
+}
+
+function acceptsCookies() {
+	document.cookie = `${ACCEPTS_COOKIES_KEY}=1`;
+	addAnalytics();
+	hideCookiesAlert();
+}
+
+function denyCookies() {
+	document.cookie = `${ACCEPTS_COOKIES_KEY}=0`;
+	hideCookiesAlert();
+}
+
+function enableCookiesClickListeners() {
+	const acceptsButton = document.getElementById("cookies-accept-button");
+	const denyButton = document.getElementById("cookies-deny-button");
+
+	acceptsButton.addEventListener("click", acceptsCookies);
+	denyButton.addEventListener("click", denyCookies);
+}
+
+function initCookies() {
+	enableCookiesClickListeners();
+	askPermissionForAnalytics();
+}
+
+window.addEventListener("load", initCookies);
