2678ffb2e60928c3a9b93ede3f19437b8427db07
Mirco Bellagamba <mirco.bellag@gmail.com>
2023-07-03 08:43:19 +0200

    Register noop service worker

--- public/js/cookies.js
+++ public/js/cookies.js
@@ -96,19 +96,31 @@ function enableCookiesClickListeners() {
 	denyButton.addEventListener("click", denyCookies);
 }
 
-function unregisterServiceWorker() {
-	navigator.serviceWorker.getRegistrations().then(function(registrations) {
-		for(let registration of registrations) {
-			registration.unregister();
+const registerServiceWorker = async () => {
+	if ("serviceWorker" in navigator) {
+		try {
+			const registration = await navigator.serviceWorker.register("/sw.js", {
+				scope: "/",
+			});
+			if (registration.installing) {
+				console.log("Service worker installing");
+			} else if (registration.waiting) {
+				console.log("Service worker installed");
+			} else if (registration.active) {
+				console.log("Service worker active");
+			}
+		} catch (error) {
+			console.error(`Registration failed with ${error}`);
 		}
-	});
+	}
+};
 
-}
+registerServiceWorker();
 
 function initCookies() {
 	enableCookiesClickListeners();
 	askPermissionForAnalytics();
-	unregisterServiceWorker();
+	registerServiceWorker();
 }
 
 window.addEventListener("load", initCookies);
