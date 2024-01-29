6a0eabbed2dd55d0010a904001420a90b3678374
Mirco Bellagamba <mirco.bellag@gmail.com>
2024-01-29 09:28:58 +0100

    Obfuscate email

--- _data/metadata.js
+++ _data/metadata.js
@@ -2,19 +2,21 @@ module.exports = {
 	title: "Mirco Bellagamba",
 	url: "https://mircobellagamba.com/",
 	language: "en",
-	description: "Mirco Bellagamba builds high performance, interactive applications, using web technologies. Front-end and app engineer at Madisoft. He builds software for humans.",
+	description:
+		"Mirco Bellagamba builds high performance, interactive applications, using web technologies. Front-end and app engineer at Madisoft. He builds software for humans.",
 	image: "/img/cover.jpg",
 	author: {
 		name: "Mirco Bellagamba",
 		email: "mirco.bellag@gmail.com",
+		emailParts: ["mirco", ".bellag", "@gmail", ".com"],
 		url: "https://mircobellagamba.com/about-me/",
 		role: "Software engineer",
 		social: {
-      twitter: `mircoBellaG`,
-      linkedin: `mirco-bellagamba`,
-      github: `mbellagamba`,
-      dev: `mbellagamba`,
-    },
+			twitter: `mircoBellaG`,
+			linkedin: `mirco-bellagamba`,
+			github: `mbellagamba`,
+			dev: `mbellagamba`,
+		},
 	},
 	now: () => new Date(),
 };
